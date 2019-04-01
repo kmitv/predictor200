e82fc749b185bc9f6a22782d4b80b31367c3cd8b
kmitv <wojtek.kmita@hotmail.com>
2019-04-01 19:24:07 +0200

    added id to serializer

--- howdy/models.py
+++ howdy/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Experience(models.Model):
+    id = models.CharField(max_length=100)
     post = models.CharField(max_length=100)
     salary = models.CharField(max_length=100)
 
