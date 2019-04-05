3fa3e4428a14afbe575f0bd8deb8f88436c6adfa
kmitv <wojtek.kmita@hotmail.com>
2019-04-05 13:45:06 +0200

    sp

--- howdy/views.py
+++ howdy/views.py
@@ -115,11 +115,11 @@ def SalaryPrediction(request, *args, **kwargs):
 
         queryset = str(kwargs['pk'])
 
-        predicted = queryset * 69
+        predicted = int(queryset) * 69
 
         # queryset = "5000"
 
-        return HttpResponse(predicted)
+        return HttpResponse(str(predicted))
 
         # def get_queryset(self):
         #         queryset = int(self.kwargs['pk'])
